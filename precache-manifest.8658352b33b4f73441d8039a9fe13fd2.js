d905c9ddc0bd5ea4707f0ce493102c897c82e6ef
Luiz <dev.luizh@gmail.com>
2018-11-09 13:18:34 -0200

    Updates

---
+++ precache-manifest.8658352b33b4f73441d8039a9fe13fd2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a6d370c0ab01e894c33",
+    "url": "/worthit/static/js/runtime~main.7a6d370c.js"
+  },
+  {
+    "revision": "3bdcdc4bc60989686f6b",
+    "url": "/worthit/static/js/main.3bdcdc4b.chunk.js"
+  },
+  {
+    "revision": "b23b7746bd4c5ae4d371",
+    "url": "/worthit/static/js/1.b23b7746.chunk.js"
+  },
+  {
+    "revision": "469a1c1b2ee53a487f893328c52e1a63",
+    "url": "/worthit/index.html"
+  }
+];
